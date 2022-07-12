3b2d46a413affe7ef91c8f39e1255a596f5e99e9
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-12 19:35:47 +0530

    Update EmailLink.js

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -82,7 +82,7 @@ const EmailLink = ({ loopMessage }) => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+      <a href={validateText(message) ? `mailto:${message}mayukhmalidas322@gmail.com` : ''}>
         <span>{message}</span>
         <span>@mldangelo.com</span>
       </a>
