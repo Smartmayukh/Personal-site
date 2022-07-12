f0bb6776a88fb95dcbc9aa716a2724a3fb21f4c0
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-12 19:46:29 +0530

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,9 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Jadavpur University',
+    degree: 'B.S. Electronics and Telecommunications Engineering',
+    link: 'http://www.jaduniv.edu.in/',
+    year: 2023,
   },
 ];
 
