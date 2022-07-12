971bd40e9a6b0cc8a214ae4877b5f52d7a13f717
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-12 20:14:19 +0530

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,7 +11,11 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">Mayukhmali Das Portfolio</Link></h2>        
+          <h2 data-testid="heading"><Link to="/">Mayukhmali Das Portfolio</Link></h2>
+          <p>
+            A beautiful, responsive, statically-generated,
+            react application written with modern Javascript.
+          </p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
