f864d08e4c007e351a0becfbbf8ac514d707fd09
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-12 19:29:23 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact Mayukhmali Das via email @ mayukhmalidas322@gmail.com"
   >
     <article className="post" id="contact">
       <header>
