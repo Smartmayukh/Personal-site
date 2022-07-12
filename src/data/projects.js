e6bed5121b440605641d7598c2fbd2d09fa1badf
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-12 21:31:42 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,38 +8,7 @@ const data = [
     desc:
     'Experimental comparisons on several publicly available datasets demonstrate that our proposed algorithm surpasses multiple state-of-the-art techniques by a maximum margin of 54 %. We further show that this algorithm consistently retrieves the missing data even during massive data loss situations.',
   },
-  {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
-  },
+
 ];
 
 export default data;
