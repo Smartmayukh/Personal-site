1227f2400965be3c6f9c3783f622c2c4727ce493
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-13 08:42:43 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const data = [
   {
     title: 'Recovery of Missing Data in Sensor Networks using Graph Signal Processing',
     subtitle: '2022 EUSIPCO',
-    image: '/images/projects/project1.png',
+    image: '/images/projects/project3.jpg',
     date: '2022-11-20',
     desc:
     'Experimental comparisons on several publicly available datasets demonstrate that our proposed algorithm surpasses multiple state-of-the-art techniques by a maximum margin of 54 %. We further show that this algorithm consistently retrieves the missing data even during massive data loss situations.',
