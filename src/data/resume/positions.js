5c014887d4337ba1bc9b8076ff8d3de48d901fdd
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-13 20:39:45 +0530

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -9,5 +9,16 @@ const positions = [
       'I have experience in Memristor based Design. Designed Vector Matrix Mulipliers using Memristor Crossbars. Working on deploying NN in Memristor crossbar arrays using Xilinx Spartan-6 FPGA',
     ],
   },
+  {
+    company: 'RF Energy Harvesting and RF Circuit Design',
+    position: 'Digital Signal Processing Lab, Jadavpur University',
+    link: 'http://www.jaduniv.edu.in/',
+    daterange: 'December 2020 - Present',
+    points: [
+      'Working on RF Energy Harvesting under the supervision of Professor Sudipta Chattopadhyay and guidance of Chandrima Thakur',
+      'Worked on various relaying protocols like TSR, PSR. Current works involve designing a modified state-of-the-art Solar-RF Harvesting System; application of relaying protocols and Solar-RF Energy harvesting on UAVs , sensors etc and using SBSP ( Space-Based Solar Power ) for RF Energy Harvesting.',
+      'Working on designing Low Noise Amplifiers and high speed ADC in Cadence',  
+    ],
+  },
 ];
 export default positions;
