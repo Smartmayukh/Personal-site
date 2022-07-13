3a12300ce7c8d62b52855d5435f5a236e97cd76a
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-13 20:44:51 +0530

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -17,7 +17,7 @@ const positions = [
     points: [
       'Working on RF Energy Harvesting under the supervision of Professor Sudipta Chattopadhyay and guidance of Chandrima Thakur',
       'Worked on various relaying protocols like TSR, PSR. Current works involve designing a modified state-of-the-art Solar-RF Harvesting System; application of relaying protocols and Solar-RF Energy harvesting on UAVs , sensors etc and using SBSP ( Space-Based Solar Power ) for RF Energy Harvesting.',
-      'Working on designing Low Noise Amplifiers and high speed ADC in Cadence',  
+      'Working on designing Low Noise Amplifiers and high speed ADC in Cadence',
     ],
   },
 ];
