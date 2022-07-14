a609b10e243a13b833ae50a7346cfe69626b7a5c
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-14 08:58:16 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ const data = [
     image: '/images/projects/project1.jpg',
     date: '2022-07-15',
     desc:
-    '',
+    'Inspired by the emerging field of graph signal processing, this paper performs a new study of a Sobolev reconstruction algorithm in wireless sensor networks',
   },
   {
     title: 'Reinforced Swarm Optimization Algorithm (2021 EUROCON)',
@@ -13,7 +13,7 @@ const data = [
     image: '/images/projects/project2.JPG',
     date: '2021-09-15',
     desc:
-    '',
+    'In this paper, we propose a novel feature selection algorithm named Reinforced Swarm Optimization (RSO) which embeds the widely used Bee Swarm Optimization (BSO) algorithm along with Reinforcement Learning (RL) to maximize the reward of a superior search agent and punish the inferior ones',
   },
   {
     title: 'Moving Object Detection for Event-based Vision (2022 UPCON)',
@@ -21,7 +21,7 @@ const data = [
     image: '/images/projects/project3.JPG',
     date: '2021-12-15',
     desc:
-    '',
+    'In this paper we use k-Means clustering for detecting moving objects in event-based data',
   },
   {
     title: 'Monocular Depth Estimation (2021 UPCON)',
@@ -29,7 +29,7 @@ const data = [
     image: '/images/projects/project4.JPG',
     date: '2021-03-15',
     desc:
-    '',
+    'In this paper, we propose a simple encoder-decoder based network that can predict high-quality depth images from single RGB images using transfer learning',
   },
 ];
 
