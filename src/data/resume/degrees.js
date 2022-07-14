2cf2fa8065bb576669d82acfa199a8c14fe82d75
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-14 19:39:11 +0530

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -5,6 +5,18 @@ const degrees = [
     link: 'http://www.jaduniv.edu.in/',
     year: 2023,
   },
+  {
+    school: 'Nava Nalanda',
+    degree: 'Class 12 (State Rank 6)',
+    link: 'https://www.navanalanda.org.in/',
+    year: 2019,
+  },
+  {
+    school: 'Nava Nalanda',
+    degree: 'Class 10 (State Rank 16)',
+    link: 'https://www.navanalanda.org.in/',
+    year: 2017,
+  },
 ];
 
 export default degrees;
