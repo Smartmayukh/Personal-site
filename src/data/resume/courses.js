43786df7abd874dd5118cf97b9f446a0fdead09f
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-14 16:35:15 +0530

    Update courses.js

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -59,6 +59,60 @@ const courses = [
     link: 'http://www.jaduniv.edu.in/',
     university: 'Jadavpur',
   },
+  {
+    title: 'Analog Circuits II',
+    number: 'B/T/221',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Digital Circuits and Systems',
+    number: 'B/T/222',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Analog Communication Systems',
+    number: 'B/T/223',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Transmission Lines and Waveguides',
+    number: 'B/T/224',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Data Structures and Algorithms',
+    number: 'B/T/225',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Analog Circuits II Lab',
+    number: 'B/S/221',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Digital Circuits II Lab',
+    number: 'B/S/222',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Analog Communication Lab',
+    number: 'B/S/223',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
+  {
+    title: 'Data Structures Lab',
+    number: 'B/S/224',
+    link: 'http://www.jaduniv.edu.in/',
+    university: 'Jadavpur',
+  },
 ];
 
 export default courses;
