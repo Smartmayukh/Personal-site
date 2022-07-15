1c9c71a2a06f5a9910c3c55d879c83841829f1ec
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-15 12:08:14 +0530

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,23 +5,24 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Mayukhmali Das's personal website. Jadavpur University undergraduate, "
-    + 'ceo of Faltung Logic Technology'}
+    description={"Mayukhmali Das's personal website. Jadavpur University undergraduate, "}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">Mayukhmali Das Portfolio</Link></h2>
+            <p>
+            A react based website written with modern Javascript.
+           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> Hello !! Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source code available <a href="https://github.com/smartmayukh/personal-site">here</a>.</p>
     </article>
   </Main>
 );
