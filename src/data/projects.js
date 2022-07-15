be88baf617e337f2649ca09830449bc4570826f2
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-15 08:51:29 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 const data = [
   {
     title: 'Recovery of Missing Data in Sensor Networks using Graph Signal Processing (2022 EUSIPCO)',
-    subtitle: '2022 EUSIPCO',
+    subtitle: 'EUSIPCO',
     image: '/images/projects/project1.jpg',
     date: '2022-07-15',
     desc:
@@ -9,7 +9,7 @@ const data = [
   },
   {
     title: 'Reinforced Swarm Optimization Algorithm (2021 EUROCON)',
-    subtitle: '2021 EUROCON',
+    subtitle: 'EUROCON',
     image: '/images/projects/project2.JPG',
     date: '2021-09-15',
     desc:
@@ -17,7 +17,7 @@ const data = [
   },
   {
     title: 'Moving Object Detection for Event-based Vision (2022 UPCON)',
-    subtitle: '2022 UPCON',
+    subtitle: 'UPCON',
     image: '/images/projects/project3.JPG',
     date: '2021-12-15',
     desc:
@@ -25,7 +25,7 @@ const data = [
   },
   {
     title: 'Monocular Depth Estimation (2021 UPCON)',
-    subtitle: '2021 UPCON',
+    subtitle: 'UPCON',
     image: '/images/projects/project4.JPG',
     date: '2021-03-15',
     desc:
