eae00c03a28fd7446124a6a1ba025e005107a195
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-19 07:30:51 +0530

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -6,13 +6,13 @@ const degrees = [
     year: 2023,
   },
   {
-    school: 'Nava Nalanda',
+    school: 'Nava Nalanda (WCHSE)',
     degree: 'Class 12 (State Rank 6)',
     link: 'https://www.navanalanda.org.in/',
     year: 2019,
   },
   {
-    school: 'Nava Nalanda',
+    school: 'Nava Nalanda (WBBSE)',
     degree: 'Class 10 (State Rank 16)',
     link: 'https://www.navanalanda.org.in/',
     year: 2017,
