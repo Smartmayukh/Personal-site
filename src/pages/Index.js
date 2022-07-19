803143aa8b9856c2e3810a97be77580bbd7fbb49
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-07-19 06:21:51 +0530

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -16,12 +16,10 @@ const Index = () => (
           </p>
         </div>
       </header>
-      <p> Hello !! Welcome to my website. 
-      
-     
-      Please feel free to read more <Link to="/about">about me</Link>
-      Here you can learn about how I developed interest in AI and Electronics. Also you will find in this page, my articles and publications, my hobbies, 
-      my travelling pictures and last but not the least some of my favourite books.
+        <p> Hello !! Welcome to my website. 
+        Please feel free to read more <Link to="/about">about me</Link>
+        Here you can learn about how I developed interest in AI and Electronics. Also you will find in this page, my articles and publications, my hobbies,
+        my travelling pictures and last but not the least some of my favourite books.
         You can also check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
