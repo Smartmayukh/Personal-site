8ef5c4a58095ea18babc63b8de48b760d02ca699
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2022-12-13 20:05:54 +0530

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -3,9 +3,9 @@ const positions = [
     company: 'RF Energy Harvesting and RF Circuit Design',
     position: 'DSP Lab, Jadavpur University',
     link: 'http://www.jaduniv.edu.in/',
-    daterange: 'December 2020 - Present',
+    daterange: 'December 2020 - December 2022',
     points: [
-      'Working on RF Energy Harvesting under the supervision of Professor Sudipta Chattopadhyay and guidance of Chandrima Thakur',
+      'Worked on RF Energy Harvesting under the supervision of Professor Sudipta Chattopadhyay and guidance of Chandrima Thakur',
       'Worked on various relaying protocols like TSR, PSR. Current works involve designing a modified state-of-the-art Solar-RF Harvesting System; application of relaying protocols and Solar-RF Energy harvesting on UAVs , sensors etc and using SBSP ( Space-Based Solar Power ) for RF Energy Harvesting',
       'Currently designing Low Noise Amplifiers and 10 Bit Low Power 1-GSPs SAR-ADC using 45nm technology in Cadence',
     ],
@@ -14,20 +14,9 @@ const positions = [
     company: 'Neurocomputing and Harware Security',
     position: 'SMDP Lab, Jadavpur University',
     link: 'http://www.jaduniv.edu.in/',
-    daterange: 'April 2021 - Present',
+    daterange: 'April 2021 - November 2022',
     points: [
-      'Currently designing Hardware Neural Networks using Digital, Analog, and FPGA based circuit design with research scholar Swarup Dandapat and under the supervision of Professor Sayan Chatterjee. FPGA related work is mainly done using Xilinx XC7A100T. I also have experience in Memristor crossbar array based designs like Vector Matrix Mulipliers',
-      'I have also done research work in implementation of Hardware trojan in FPGA based designs. One of my papers entitled as ”Logic and Reduction Operation based Hardware Trojans in Digital Design” is currently under review in ISOCC 2022',
-    ],
-  },
-  {
-    company: 'Embedded System',
-    position: 'Texas Instruments Lab, Jadavpur University',
-    link: 'http://www.jaduniv.edu.in/',
-    daterange: 'May 2021 - Present',
-    points: [
-      'Working on Lidar-based research under the supervision of Dr. P. Venkateswaran',
-      'This research project mainly focuses on designing a autonomous boat that can do underwater mapping using a Lidar.',
+      'I have done research work in implementation of Hardware trojan in FPGA based designs. One of my papers entitled as ”Logic and Reduction Operation based Hardware Trojans in Digital Design” has benn published in ISOCC 2022',
     ],
   },
   {
@@ -36,8 +25,7 @@ const positions = [
     link: 'http://www.jaduniv.edu.in/',
     daterange: 'December 2021 - Present',
     points: [
-      'Working on implementation of complete communication system models with modulation techniques such as OFDM in FPGA. This work is a part of my final year project under the supervision of Professor Iti Saha Misra',
-      'Future plans of this project include using neural networks for OFDM channel estimation and improving Bit Error Rate',
+      'Working on implementation of complete communication system models with modulation techniques such as OFDM. This work is a part of my final year project under the supervision of Professor Iti Saha Misra',
     ],
   },
 
@@ -45,26 +33,16 @@ const positions = [
     company: 'Electromagnetic Energy Harvesting',
     position: 'Advanced Technology Development Centre, IIT Kharagpur',
     link: 'http://iitkgp.ac.in/',
-    daterange: 'June 2022 - Present',
+    daterange: 'June 2022 - September 2022',
     points: [
-      'This is my summer internship under the supervision of Banibrata Mukherjee.In this project I am working on Electromagnetic Energy Harvesting in vehicles where I am designing both the harvester as well as the associated circuits which include sub circuits like Synchronized Switch Harvesting on Inductor, Boost Converter with feedforward and feedback. Also working on application of MPPT algorithm on Electromagnetic Energy Harvesting.',
-    ],
-  },
-  {
-    company: 'Computational Biology',
-    position: 'Carnegie Mellon University, Pittsburgh, United States',
-    link: 'https://www.cmu.edu/',
-    daterange: 'March 2021 - July 2021',
-    points: [
-      'Worked in XU Lab under Professor Min Xu . The topic which I worked on was COVID-19 Cryo-ET data analysis',
-      'Worked on tasks like PDB to MRC file conversion using EMAN2 and AITom. Compared the results of such files. Also identified the sub-tomograms using DoG particle picking. I have also worked on template matching. Performed saliency detection on the Cryo-ET tomograms using PUB-SalNet. Trained various semi-supervised and supervised models.',
+      'This is my summer internship under the supervision of Banibrata Mukherjee.In this project I worked on Electromagnetic Energy Harvesting in vehicles where I designed both the harvester as well as the associated circuits which include sub circuits like Synchronized Switch Harvesting on Inductor, Boost Converter with feedforward and feedback. Also worked on application of MPPT algorithm on Electromagnetic Energy Harvesting.',
     ],
   },
   {
     company: 'Computer Vision',
     position: 'CMATER Lab',
     link: 'http://www.jaduniv.edu.in/',
-    daterange: 'September 2020 - Present',
+    daterange: 'September 2020 - December 2022',
     points: [
       'Worked on Monocular Depth Estimation. One paper regarding this topic is already published.',
       'Worked on detection of moving object in event-based vision using graph-based methods. Corresponding paper has been published',
@@ -74,9 +52,9 @@ const positions = [
     company: 'Anomaly Detection',
     position: 'Texas Instruments Lab',
     link: 'http://www.jaduniv.edu.in/',
-    daterange: 'December 2020 - Present',
+    daterange: 'December 2020 - June 2022',
     points: [
-      'Worked on Anomaly Detection in Wireless Sensor Networks under Dr. P. Venkateswaran. Our paper entitled ”Recovery of Missing Sensor Data by Reconstructing Time-varying Graph Signals” got selected for publication in EUSIPCO 2022',
+      'Worked on Anomaly Detection in Wireless Sensor Networks under Dr. P. Venkateswaran. Our paper entitled ”Recovery of Missing Sensor Data by Reconstructing Time-varying Graph Signals” has been published in EUSIPCO 2022',
     ],
   },
   {
@@ -85,8 +63,8 @@ const positions = [
     link: 'http://www.jaduniv.edu.in/',
     daterange: 'December 2020 - May 2021',
     points: [
-      'Working on Natural Language Processing under the supervision of Dr. Sudip Kumar Naskar',
-      'Topics of research include Sentiment Analysis in Regional Languages and Aspect analysis. Worked with various transformer models like Bert , XLNet and their modifications . Current research papers are mainly focused on new and modified TF-IDF vectoriser ; Spectral clustering and its application to NLP ; Merging various types of word vector embeddings from fastText , Word2Vec , GloVe , TF-IDF and Bert for improved accuracy in unsupervised classification of text',
+      'Worked on Natural Language Processing under the supervision of Dr. Sudip Kumar Naskar',
+      'Topics of research include Sentiment Analysis in Regional Languages and Aspect analysis. Worked with various transformer models like Bert , XLNet and their modifications',
     ],
   },
   {
